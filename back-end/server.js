6ef7765ac4af5774394093a5e6ed629cd45b2d16
vrramosm <vf.ramos7@gmail.com>
2020-06-16 18:37:10 -0300

    Adicionado o script para a criaçao do banco de dados e criado a pasta index.js e server.js para a inicializaçao do projeto

---
+++ back-end/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, 'public', 'images')));
+
+module.exports = app;
