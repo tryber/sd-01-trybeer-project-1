393c12d611a3c5883bec5d0227c589f889203af7
henriqueeyer <henriqueeyer@outlook.com>
2020-06-17 16:28:31 -0300

    Update sql

--- script.sql
+++ script.sql
@@ -49,18 +49,22 @@ VALUES
 
 INSERT INTO users (name, password, email, role)
 VALUES
-('Henrique', 123456, 'henrique@gmail.com', 'admin');
+('Henrique', 123456, 'henrique@gmail.com', 'admin'),
+('Coruja', 123456, 'henrique@gmail.com', 'admin');
 
 INSERT INTO orders (id_user, address)
 VALUES
 (1, 'Belo Horizonte'),
-(1, 'São Paulo');
+(1, 'São Paulo'),
+(2, 'São Paulo');
 
 INSERT INTO orders_products (id_order, id_product, quantity)
 VALUES
 (1, 1, 12),
 (1, 5, 6),
-(2, 3, 2);
+(2, 3, 2),
+(3, 5, 10),
+(2, 1, 10);
 
 USE trybeer;
 SELECT P.name_product, P.price, OP.quantity, P.price * OP.quantity AS Total
@@ -70,3 +74,130 @@ ON P.id_product = OP.id_product
 INNER JOIN orders AS O
 ON O.id_order = OP.id_order
 WHERE O.id_order = 2;
+
+-- PROCEDURE 
+
+DELIMITER $$
+CREATE PROCEDURE `createUser`(IN nameValue VARCHAR(80),IN emailValue VARCHAR(50),IN passwordValue VARCHAR(64),IN roleValue VARCHAR(20))
+BEGIN
+INSERT INTO users (name, email,password, role)
+VALUES
+(nameValue, emailValue, passwordValue, roleValue);
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `createOrder`(IN idUser INT, IN address VARCHAR(255))
+BEGIN
+INSERT INTO orders (id_user, address)
+VALUES
+(idUser, address);
+SELECT id_order FROM orders order by id_order desc limit 1;
+END$$
+DELIMITER ;
+
+call createOrder(3,'Rua show');
+call getListProducts()
+
+DELIMITER $$
+CREATE PROCEDURE `createProductOrder`(IN idOrder INT, IN idProducts INT, IN qtdValue INT)
+BEGIN
+INSERT INTO orders_products (id_order, id_product, quantity)
+VALUES
+(idOrder, idProducts, qtdValue);
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `getAllDataOrder`()
+BEGIN
+SELECT O.id_order, O.data, O.address, U.name AS client, O.status, priceOrderTotal(id_order) AS Total
+FROM orders AS O
+INNER JOIN users AS U
+ON O.id_user = U.id_user;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `getAllDataOrderUser`(IN idUser INT)
+BEGIN
+SELECT O.id_order, O.data, O.address, priceOrderTotal(id_order) AS Total
+FROM orders AS O
+WHERE O.id_user = idUser;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `getListProducts`()
+BEGIN
+SELECT * FROM products;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `getProductsInOrder`(IN idOrder INT)
+BEGIN
+SELECT P.name_product, P.price, OP.quantity, P.price * OP.quantity AS Total
+FROM products AS P
+INNER JOIN orders_products AS OP
+ON P.id_product = OP.id_product
+INNER JOIN orders AS O
+ON O.id_order = OP.id_order
+WHERE O.id_order = idOrder;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `getUniqueDataOrder`(IN idOrder INT)
+BEGIN
+SELECT O.id_order, O.data, O.address, U.name AS client, O.status, priceOrderTotal(id_order) AS Total
+FROM orders AS O
+INNER JOIN users AS U
+ON O.id_user = U.id_user
+WHERE O.id_order = idOrder;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `getUser`(IN user INT)
+BEGIN
+SELECT email, name FROM users
+WHERE id_user = user;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `updateStatusOrder`(IN idOrder INT, IN valueStatus TINYINT)
+BEGIN
+UPDATE orders O
+	SET O.status = valueStatus
+WHERE O.id_order = idOrder;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `updateUser`(IN idUser INT, IN name_Value VARCHAR(80))
+BEGIN
+UPDATE users s
+	SET s.name = name_Value
+WHERE s.id_user = idUser;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE FUNCTION `priceOrderTotal`(idOrder INT) RETURNS double
+    READS SQL DATA
+BEGIN
+DECLARE sum_total DOUBLE;
+SELECT SUM(P.price * OP.quantity) AS Total
+FROM products AS P
+INNER JOIN orders_products AS OP
+ON P.id_product = OP.id_product
+INNER JOIN orders AS O
+ON O.id_order = OP.id_order
+WHERE O.id_order = idOrder INTO sum_total;
+RETURN sum_total;
+END$$
+DELIMITER ;
+
+
