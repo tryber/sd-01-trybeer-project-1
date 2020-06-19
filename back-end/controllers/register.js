060a3a6b0856f1f1fe2d6f4b8647a8b0856adcf4
vrramosm <vf.ramos7@gmail.com>
2020-06-19 16:33:25 -0300

    Mudado alguns trechos de codigo do controller para o model

--- back-end/controllers/register.js
+++ back-end/controllers/register.js
@@ -1,13 +1,10 @@
+const { registerUserDB } = require('../models/appBeer');
 const { encrypt } = require('../services/crypto');
-const { connectionPromise } = require('../services/distributor');
 
 exports.register = async (req, res) => {
   const { name, email, password, role } = req.body;
-
   const ecryptedPassword = encrypt(password);
-
-  const query = `call createUser("${name}", "${email}", "${ecryptedPassword}", "${role}")`;
-  const userData = await connectionPromise(query);
+  const userData = await registerUserDB(name, email, ecryptedPassword, role);
 
   if (userData) return res.status(200).json({ message: 'Registered successfully.' });
 }
--- back-end/models/appBeer.js
+++ back-end/models/appBeer.js
@@ -1,5 +1,11 @@
-const connection = require('./connectionModel');
+const { connectionPromise } = require('../services/distributor');
 
-const registerUserDB() {
-  
+const registerUserDB = async (name, email, ecryptedPassword, role) => {
+  const query = `call createUser("${name}", "${email}", "${ecryptedPassword}", "${role}")`;
+  const data = await connectionPromise(query);
+  return data;
+}
+
+module.exports = {
+  registerUserDB,
 }
