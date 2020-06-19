2232e98967ee271e865236c41d809035e0cd2dc4
vrramosm <vf.ramos7@gmail.com>
2020-06-19 18:30:38 -0300

    refatorado o arquivo controller/register.js

--- back-end/controllers/register.js
+++ back-end/controllers/register.js
@@ -4,7 +4,7 @@ const { encrypt } = require('../services/crypto');
 exports.register = async (req, res) => {
   const { name, email, password, role } = req.body;
   const ecryptedPassword = encrypt(password);
-  const userData = await appBeer.registerUserDB(name, email, ecryptedPassword, role);
+  await appBeer.registerUserDB(name, email, ecryptedPassword, role);
 
-  if (userData) return res.status(200).json({ message: 'Registered successfully.' });
+  res.status(200).json({ message: 'Registered successfully.' });
 };
