d89760e02bc40b6e33be583f2a3e02d6761e0912
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-06-23 12:05:22 -0300

    colocando validação JWT dentro de services

--- back-end/middlewares/userValid.js
+++ back-end/middlewares/userValid.js
@@ -1,16 +1,5 @@
-const path = require('path');
-const jwt = require('jsonwebtoken');
-
 const { errorReadingJWT } = require('../rescue/rescues');
-
-const enviromentVariable = path.resolve(__dirname, '..', '..', '.env');
-
-require('dotenv').config({ path: enviromentVariable });
-
-const tokenValid = (token) => {
-  const payload = jwt.verify(token, process.env.SECRET_KEY_JWT);
-  return payload;
-};
+const tokenValid = require('../services/validJWT');
 
 const userValidMiddleware = errorReadingJWT((req, res, next) => {
   const token = req.headers.authorization;
@@ -30,6 +19,5 @@ const userValidMiddleware = errorReadingJWT((req, res, next) => {
 });
 
 module.exports = {
-  tokenValid,
   userValidMiddleware,
 };
