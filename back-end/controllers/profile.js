b81bad722a0e48f97ced3b5ae3ad993f562ecaf7
vrramosm <vf.ramos7@gmail.com>
2020-06-24 18:50:43 -0300

    Otimizado a rota /profile

--- back-end/controllers/profile.js
+++ back-end/controllers/profile.js
@@ -4,9 +4,9 @@ const appBeer = require('../models/appBeer');
 exports.profile = async (req, res) => {
   const { name } = req.body;
   const token = req.headers.authorization;
-  const unencryptedToken = tokenValid(token);
+  const { email } = tokenValid(token);
 
-  const updatedUser = await appBeer.getUser(unencryptedToken.email, name);
+  const updatedUser = await appBeer.getUser(email, name);
 
   return res.status(200).json(updatedUser);
 };
