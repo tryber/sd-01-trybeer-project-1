48073b7d0740f75f32e688c511c5c319664af029
henriqueeyer <henriqueeyer@outlook.com>
2020-06-24 16:15:43 -0300

    foi

---
+++ front-end/src/pages/Products.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Provider } from './context/index';
+// import Register from './pages/Register';
+import Login from './pages/Login';
+
+function Products() {
+  return (
+    <Provider>
+      <Headers />
+    </Provider>
+  );
+}
+
+export default Products;
