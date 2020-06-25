328a7f6e7e9b51022cd8927e9696c76c85efdb80
henriqueeyer <henriqueeyer@outlook.com>
2020-06-25 18:26:27 -0300

    foi

--- front-end/src/context/index.js
+++ front-end/src/context/index.js
@@ -6,6 +6,14 @@ import { saveCar, getCar } from '../service/CarBuyer';
 
 const TrybeerContext = createContext();
 
+const resetUser = (res, setIsError, setUser) => {
+  if (res.error) {
+    clearUser();
+    setUser();
+    return setIsError(true);
+  }
+}
+
 const TrybeerProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
   const [user, setUser] = useState();
@@ -16,11 +24,7 @@ const TrybeerProvider = ({ children }) => {
     if (!isFetching && user) {
       setIsFetching(true);
       fetchApi(requestWithToken(user)).then((res) => {
-        if (res.error) {
-          clearUser();
-          setUser();
-          return setIsError(true);
-        }
+        if (res.error) return resetUser(res, setIsError, setUser)
         setProducts(res);
         setIsFetching(false);
       })
@@ -56,3 +60,71 @@ export { TrybeerContext, TrybeerProvider as Provider };
 TrybeerProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
+
+// import React, { useState, createContext } from 'react';
+// import PropTypes from 'prop-types';
+// import { fetchApi, requestWithToken } from '../service/serviceFetch';
+// import { clearUser } from '../service/index';
+// import { saveCar, getCar } from '../service/CarBuyer';
+
+// const TrybeerContext = createContext();
+
+// const getAllProducts = ({ isFetching, user, setIsFetching, setUser, setIsError, setProducts, setIsFetching }) => {
+//   if (!isFetching && user) {
+//     setIsFetching(true);
+//     fetchApi(requestWithToken(user)).then((res) => {
+//       if (res.error) {
+//         clearUser();
+//         setUser();
+//         return setIsError(true);
+//       }
+//       setProducts(res);
+//       setIsFetching(false);
+//     })
+//   }
+// }
+
+// const updateCarBuyer = ({ setCarBuyer }, obj) => {
+//   saveCar(obj);
+//   setCarBuyer(obj);
+// }
+
+// const localCarBuyer = ({ setCarBuyer }) => (getCar()) ? setCarBuyer(getCar()) : setCarBuyer({ list: [], total: 0, });
+
+// const TrybeerProvider = ({ children }) => {
+//   const [products, setProducts] = useState([]);
+//   const [user, setUser] = useState();
+//   const [isError, setIsError] = useState(false);
+//   const [isFetching, setIsFetching] = useState(false)
+//   const [carBuyer, setCarBuyer] = useState({ list: [], total: 0, });
+
+//   const miniContext = {
+//     carBuyer,
+//     user,
+//     products,
+//     isFetching,
+//     isError,
+//     setIsError,
+//     setUser,
+//     setProducts,
+//     setIsFetching,
+//     setCarBuyer
+//   }
+//   const context = {
+//     ...miniContext,
+//     saveCarBuyer: (value) => updateCarBuyer(miniContext, value),
+//     fetchProducts: () => getAllProducts(miniContext),
+//     verifyCarBuyer: () => localCarBuyer(miniContext)
+//   };
+//   return (
+//     <TrybeerContext.Provider value={context}>
+//       {children}
+//     </TrybeerContext.Provider>
+//   );
+// };
+
+// export { TrybeerContext, TrybeerProvider as Provider };
+
+// TrybeerProvider.propTypes = {
+//   children: PropTypes.node.isRequired,
+// };
