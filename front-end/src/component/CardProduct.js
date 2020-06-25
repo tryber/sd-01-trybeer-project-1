e2c6664a52573afdc0da028952e2c430f2610855
henriqueeyer <henriqueeyer@outlook.com>
2020-06-25 19:17:47 -0300

    CC navbar.css

--- front-end/src/component/CardProduct.js
+++ front-end/src/component/CardProduct.js
@@ -11,15 +11,15 @@ function CardProduct({ index, attributes }) {
   const qtd = (infoProductCar) ? infoProductCar.qtd : 0;
   return (
     <div className="CardProduct">
-      <h3 data-testid={`${index}-product-price`}>{`R$ ${price.toLocaleString('pt-BR')}`}</h3>
+      <h3 className="product-price" data-testid={`${index}-product-price`}>{`R$ ${price.toLocaleString('pt-BR')}`}</h3>
       <div className="img-product">
         <img data-testid={`${index}-product-img`} src={image} alt={`Product ${name}`} />
       </div>
       <h2 data-testid={`${index}-product-name`} className="product-name">{name}</h2>
       <div className="div-qtd">
-        <input type="button" value="-" disabled={qtd === 0} data-testid={`${index}-product-minus`} onClick={() => saveCarBuyer(addItemInCarBuyer(carBuyer, { id, name, price, qtd: qtd - 1 }))} />
-        <input type="number" min="0" value={qtd} data-testid={`${index}-product-qtd`} onChange={(e) => saveCarBuyer(addItemInCarBuyer(carBuyer, { id, name, price, qtd: Number(e.target.value) }))} />
-        <input type="button" value="+" data-testid={`${index}-product-plus`} onClick={() => saveCarBuyer(addItemInCarBuyer(carBuyer, { id, name, price, qtd: qtd + 1 }))} />
+        <input type="button" className="btn-qtd" value="-" disabled={qtd === 0} data-testid={`${index}-product-minus`} onClick={() => saveCarBuyer(addItemInCarBuyer(carBuyer, { id, name, price, qtd: qtd - 1 }))} />
+        <input type="number" className="input-qtd" min="0" value={qtd} data-testid={`${index}-product-qtd`} onChange={(e) => saveCarBuyer(addItemInCarBuyer(carBuyer, { id, name, price, qtd: Number(e.target.value) }))} />
+        <input type="button" className="btn-qtd" value="+" data-testid={`${index}-product-plus`} onClick={() => saveCarBuyer(addItemInCarBuyer(carBuyer, { id, name, price, qtd: qtd + 1 }))} />
       </div>
     </div>
   );
