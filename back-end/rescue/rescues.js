f2fdb14e80efd2cf2f01315d56db0ea5696297ae
henriqueeyer <henriqueeyer@outlook.com>
2020-06-27 20:45:52 -0300

    foi

--- back-end/rescue/rescues.js
+++ back-end/rescue/rescues.js
@@ -2,7 +2,7 @@ exports.invalidLogin = fn => async (req, res, next) => {
   try {
     await fn(req, res, next);
   } catch (err) {
-    res.status(400).json({ message: 'Invalid Fields', error: err.message });
+    res.status(400).json({ message: 'Invalid Fields', error: err.message});
   }
 };
 
@@ -18,6 +18,6 @@ exports.errorReadingJWT = fn => async (req, res, next) => {
   try {
     await fn(req, res, next);
   } catch (err) {
-    res.status(422).json({ message: 'Unprocessable Entity', error: err.message });
+    res.status(422).json({ message: 'Unprocessable Entity', error: err.message, trace:err.trace  });
   }
 };
---
+++ front-end/src/pages/MyProfile.js
@@ -0,0 +1,60 @@
+import React, { useState, useContext, useEffect } from 'react';
+import Headers from '../component/Header';
+import { TrybeerContext } from '../context';
+import { fetchApi } from '../service/serviceFetch';
+import ReportComponent from '../component/ReportComponent';
+import { saveUser, getUser } from '../service';
+import '../styles/DataProfile.css';
+
+async function handleSubmit(obj, setMessageRequest, setUser) {
+  const { name, } = obj;
+  const body = {
+    name,
+  };
+  const data = await fetchApi({
+    endpoint: 'http://localhost:3001/profile',
+    method: 'POST',
+    body,
+    headers: {
+      'Content-Type': 'application/json',
+      authorization: getUser().token,
+    }
+  });
+  setMessageRequest((data.message || 'Atualizado com sucesso'));
+  if (!data.message) {
+    saveUser(data);
+    setUser(getUser());
+  }
+}
+
+function MyProfile() {
+  const { user, setUser } = useContext(TrybeerContext);
+  const [messageRequest, setMessageRequest] = useState();
+  const [name, setName] = useState('');
+  useEffect(() => {
+    if (user) setName(user.name)
+  }, [user])
+  console.log('message', messageRequest)
+  return (
+    <div className="MyProfile">
+      <Headers path="/profile" />
+      {!user ||
+        <div className="DataProfile">
+          {!messageRequest || <ReportComponent message={{ messageRequest, setMessageRequest }} />}
+          <label className="lbl-name" htmlFor="name">Nome:</label>
+          <input data-testid="profile-name-input" className="input-name" id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          <label className="lbl-name" htmlFor="name">Email:</label>
+          <div data-testid="profile-email-input" className="input-email" id="email">
+            {user.email}
+          </div>
+          <button data-testid="profile-save-btn" type="button" className="btn-save-name" onClick={() => handleSubmit({ name }, setMessageRequest, setUser)}
+          >
+            Salvar
+      </button>
+        </div>
+      }
+    </div>
+  );
+}
+
+export default MyProfile;
