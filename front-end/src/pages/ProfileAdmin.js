0034d9c207f5ddf50fef22a8229f3aee4f641d2d
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-06-26 23:06:02 -0300

    code climate

--- front-end/src/pages/ProfileAdmin.js
+++ front-end/src/pages/ProfileAdmin.js
@@ -6,29 +6,6 @@ import Profile from '../component/ProfileAdmin';
 import '../styles/ProfileAdmin.css';
 
 function ProfileAdmin() {
-  // const [isFetching, setIsFetching] = useState(true);
-  // const [data, setData] = useState();
-
-  // useEffect(() => {
-  //   const fetch = async () => {
-  //     const result = await fetchApi(requestWithToken(getUser(), 'admin/profile'));
-  //     setData(result);
-  //   };
-  //   fetch();
-  // }, []);
-
-  // // if (shouldRedirect) return <Redirect to="/" />
-  // if (!data) return 'Loading';
-
-  // return (
-  //   <div>
-  //     {/* <NavBar /> */}
-  //     <div>
-  //       <h3>{data.name}</h3>
-  //       <h3>{data.email}</h3>
-  //     </div>
-  //   </div>
-  // );
   const [{ data, loading, error }] = useAxios({
     url: 'http://localhost:3001/admin/profile',
     headers: {
