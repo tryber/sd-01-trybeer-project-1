c34da34efadd3512332e508a331174ac0b192f13
henriqueeyer <henriqueeyer@outlook.com>
2020-06-28 20:55:12 -0300

    Foi

--- front-end/src/pages/Orders.js
+++ front-end/src/pages/Orders.js
@@ -13,8 +13,8 @@ function Orders({ location: { pathname } }) {
     method: 'GET',
     headers: { authorization: verifyUser(user), }
   })
-  if (!getUser()) return <Redirect to="/" />
-  if (error || (data && data.message)) return <Redirect to="/" />
+  if (!getUser() || data.message) return <Redirect to="/" />
+  if (error) return <h2>Algo deu de errado!</h2>
   return (
     <div className="Orders">
       <Headers path={`${pathname}`} />
