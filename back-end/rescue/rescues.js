cdf89a3d0829dd8f4d05c2e983eef55ed2cf7643
henriqueeyer <henriqueeyer@outlook.com>
2020-06-27 21:36:31 -0300

    Termiando tela de perfil

--- back-end/rescue/rescues.js
+++ back-end/rescue/rescues.js
@@ -2,7 +2,7 @@ exports.invalidLogin = fn => async (req, res, next) => {
   try {
     await fn(req, res, next);
   } catch (err) {
-    res.status(400).json({ message: 'Invalid Fields', error: err.message});
+    res.status(400).json({ message: 'Invalid Fields', error: err.message });
   }
 };
 
