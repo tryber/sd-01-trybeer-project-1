b54feb3897ac1b26e23e3855f604702110b30439
henriqueeyer <henriqueeyer@outlook.com>
2020-06-28 20:47:28 -0300

    Foi

--- back-end/rescue/rescues.js
+++ back-end/rescue/rescues.js
@@ -10,7 +10,6 @@ exports.databaseErrorHandling = fn => async (req, res, next) => {
   try {
     await fn(req, res, next);
   } catch (err) {
-    console.log(err)
     res.status(500).json({ message: 'Internal Server Error', error: err.message });
   }
 };
