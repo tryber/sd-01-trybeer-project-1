988ffd4ccca4eb40142a452e04c0488f0fe4614c
vrramosm <vf.ramos7@gmail.com>
2020-07-01 10:03:22 -0300

    colocando os testid

--- front-end/src/component/cardOrdersAdmin.js
+++ front-end/src/component/cardOrdersAdmin.js
@@ -3,14 +3,15 @@ import React from 'react';
 function CardOrders({ orders }) {
   const { Total, address, address_number, data, id_order, status } = orders;
 
+  console.log(id_order);
+
   return (
     <div>
-      <p>Total: R${Total}</p>
-      <p>Endereço: {address}</p>
-      <p>Número: {address_number}</p>
+      <p data-testid={`${id_order}-order-number`}> Pedido {id_order}</p>
+      <p data-testid={`${id_order}-order-address`}>Endereço: {address}, {address_number}</p>
       <p>Data de Compra: {data}</p>
-      <p>Pedido {id_order}</p>
       <p>Status: {status === 0 ? 'Pendente' : 'Entregue' }</p>
+      <p data-testid={`${id_order}-order-total-value`}>Total: R${Total}</p>
     </div>
   );
 };
